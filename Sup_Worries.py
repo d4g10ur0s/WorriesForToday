0c2c244fbb7bdeadb725062674ba5c78baf063a7
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2020-08-05 19:42:28 +0300

    Update Sup_Worries.py

--- Sup_Worries.py
+++ Sup_Worries.py
@@ -32,6 +32,8 @@ class Item:
         return self._id
 
     #manipulating frequency
+    def set_frequency(self, fr):
+        self.fr = fr
     def calc_frequency(self):
         self.fr = self._lt.toordinal() - self._tb.toordinal()
     def get_frequency(self):
@@ -51,13 +53,14 @@ class Item:
         self._lt = str(lt)
     def last_goes_next(self):
         self._lt=self._tb
-    def next_date(self):
+    def next_date(self, on = False):
         #months with 31 days
         arr = [1,3,5,7,8,10,12]
 
         day = int(self._lt[8:]) + int(self.fr)
         month = int(self._lt[5:7])
         year = int(self._lt[:4])
+
         if (month in arr) and (day>31):
             day-=31
             month+=1
@@ -75,7 +78,11 @@ class Item:
             else:
                 pass
         #set next date
-        self._lt = self._tb
+        if on:
+            pass
+        else:
+            self._lt = self._tb
+
         self._tb = str(datetime.date(year,month,day))
 
     #manipulating basic string
@@ -98,3 +105,115 @@ class Item:
             return False
     def return_basic_string(self):
         return self._id + " " + self._tb + " " + self._lt + " " + self.fr + " " + self._name +'\n'
+################################################################################
+#Input Data
+
+#Date
+def get_inp_date():
+    inp = str(input())
+    inp = datetime.date(int(inp[:4]), int(inp[5:7]), int(inp[8:]) )
+    return inp
+#String
+def get_inp_str():
+    return str(input())
+#Int
+def get_inp_int():
+    return int(input())
+
+#Construct a Today Item via CMD
+def today_item():
+    print("Give name.\n")
+    return Item(get_inp_str())
+#Construct Using Dates
+def occured_today_but_done_date():
+    i = 0
+################################################################################
+    print("Give name.\n")
+    obj = Item(get_inp_str())
+    while i < 3:
+        try:
+            print("When is it going to be occured?\n\n(Format : yyyy-mm-dd)\n")
+            if obj.set_tb( get_inp_date() ):
+                break
+            else:
+                i+=1
+        except:
+            i+=1
+            continue
+    if i == 3:
+        #prepei na valw exception kai handling se hpshlotero epipedo
+        pass
+    else:
+        obj.calc_frequency()
+        return obj
+#Happened Today and Have a Frequency
+def occured_today_but_done_frequency():
+    i=0
+################################################################################
+    print("Give name.\n")
+    obj = Item(get_inp_str())
+    while i<3:
+        try:
+            print("Give frequency.\n")
+            obj.set_frequency(get_inp_int())
+            break
+        except:
+            i+=1
+            continue
+    obj.next_date()
+    return obj
+#Happened in the past , set with date
+def happened_past_date():
+    i = 0
+    kati = ["did","is going",""," to"]
+################################################################################
+    print("Give name.\n")
+    obj = Item(get_inp_str())
+
+    for j in range(0,2):
+        while i<3:
+            try:
+                print("When "+ kati[j] +" it "+ kati[j+2] +" occur?\n\nFormat : yyyy-mm-dd\n")
+                if j==0:
+                    obj.set_lt(get_inp_date())
+                else:
+                    obj.set_tb(get_inp_date())
+                i = 0
+                break
+            except:
+                i+=1
+                continue
+            #8elw exception gia i == 3
+    obj.calc_frequency()
+    return obj
+#Happened in the past , set with frequency
+def happened_past_frequency():
+    i = 0
+################################################################################
+    print("Give name.\n")
+    obj = Item(get_inp_str)
+    while i<3:
+        try:
+            print("When did it occur?\n\nFormat : yyyy-mm-dd\n")
+            obj.set_lt(get_inp_date())
+            i = 0
+            break
+        except:
+            i+=1
+            continue
+        #8elw exception gia i = 3
+    while i<3:
+        try:
+            print("Give frequency.\n")
+            obj.set_frequency(get_inp_int())
+            i = 0
+            break
+        except:
+            i+=1
+            continue
+        #8elw exception gia i==3
+    obj.next_date(True)
+    #prepei na valw ena exception gia to ti ginetai ann ola einai palia...
+    return obj
+
+################################################################################
